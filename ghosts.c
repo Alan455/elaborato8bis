1f188423628119d92da081778777de3ce436ea45
AlanSorrenti <77885859+Alan455@users.noreply.github.com>
2022-11-22 10:46:13 +0100

    Update ghosts.c

--- ghosts.c
+++ ghosts.c
@@ -10,37 +10,35 @@
 #include "matrix.h"
 
 static const struct position UNK_POSITION = {-1,-1}; // Set unknown position to (UINT_MAX,UINT_MAX)
+struct ghost { 
+    int id;  //questo è l’id del fantasma 
+    int status; 
+    int dir; 
+    struct position pos; 
+}; 
 
-struct ghosts {
-    int n; //numero di fantasmi
-    struct ghost ghost;
+struct ghosts { 
+    char **A; 
+    unsigned int nrow; 
+    unsigned int ncol; 
+    unsigned int n; 
+    struct ghost *ghost; 
 };
 
-
-struct ghost {
-    int id; 
-   	enum ghost_status status;
-	enum direction dir;
-	struct position pos;
-    //struct ghost 
-    //char **A;
-	unsigned int nrow;
-	unsigned int ncol;
-};
 
 /* Create the ghosts data structure */
 struct ghosts *ghosts_setup(unsigned int num_ghosts) { 
     struct ghosts *G = (struct ghosts *)malloc(sizeof(struct ghosts));
     //srand(time(NULL));
-    if(G != NULL) {                 //se G non punta a NULL
+    if(G != NULL) {  //se G non punta a NULL
         unsigned int i;
-        //G->n = num_ghosts;                                                  // al campo n di G assegniamo num_ghosts
+        G->n = num_ghosts;  // al campo n di G assegniamo num_ghosts
         G->ghost = (struct ghost *)calloc(num_ghosts,sizeof(struct ghost)); //al campo ghost di G assegniamo dinamicamente la memoria
         for(i = 0; i < G->n; i++) {                     //per ogni ghost presente assegniamo una UNK_POSITION e una direzione casuale
-            G->ghost[i]->pos   = UNK_POSITION ;
+            G->ghost->pos   = UNK_POSITION ;
             G->ghost->dir   = UNK_DIRECTION;
             G->ghost->id    = i;
-            G->ghost->status = UNK_GHOST_STATUS;*/
+            G->ghost->status = UNK_GHOST_STATUS;
         }
     }
     return G;
