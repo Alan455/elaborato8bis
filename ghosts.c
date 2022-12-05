264c0bb515e6a0cd7e67f3402379902ad9af092a
AlanSorrenti <77885859+Alan455@users.noreply.github.com>
2022-12-05 09:57:24 +0100

    Update ghosts.c

--- ghosts.c
+++ ghosts.c
@@ -101,8 +101,10 @@ enum ghost_status ghosts_get_status(struct ghosts* G, unsigned int id) {
     return UNK_GHOST_STATUS;
 }
 
-static int nearby_home(char** A, unsigned int nrow, unsigned int ncol, struct position pos) {
+static int nearby_home(struct ghosts *G, unsigned int nrow, unsigned int ncol, struct position pos) {
     int n = 2, a, b, i = pos.i, j = pos.j;
+    char** A;
+    A = G->A;
     for (a = -n; a <= n; a++)
         if (i + a >= 0 && i + a < nrow)
             for (b = -n; b <= n; b++)
@@ -113,8 +115,7 @@ static int nearby_home(char** A, unsigned int nrow, unsigned int ncol, struct po
 
 }
 
-//Controlla che i movimenti del ghost avvengano in posizioni idonee in base al suo stato
-enum direction legal_movement(struct ghosts* G, struct pacman* P, struct position pos, enum direction dir, enum ghost_status status, unsigned int id) {
+enum direction legal_dir(struct ghosts* G, struct pacman* P, struct position pos, enum direction dir, enum ghost_status status, unsigned int id) {
     struct position pacP, ghostP;
     struct position ghostP;
     int rows, cols, ghostPi, ghostPj;
@@ -133,13 +134,63 @@ enum direction legal_movement(struct ghosts* G, struct pacman* P, struct positio
     default: printf("errore nella legal movement,non è stato assegnato una direzione");
         return 4;
     }
-    if (IS_WALL(G->A, ghostP))    return 4;
-    if (IS_GHOST(G->A, ghostP))   return 4;
+    if (IS_WALL(G->A, ghostP))    return 0;
+    if (IS_GHOST(G->A, ghostP))   return 0;
     if (!(G->ghost[id].status == NORMAL) && IS_PACMAN(G->A, pacP))  return dir;
+    if (IS_PACMAN(G->A, pacP)) return 0;
     return dir;
 }
 
-struct position ghostPosition(struct ghosts* G,enum direction dir,unsigned int id) {
+enum direction choose_dir(struct ghosts* G, struct pacman* P) {
+    int i, k, id;
+    struct position pacP, ghostP;
+    enum ghost_status status;
+    id = G->ghost->id;
+    status = G->ghost[id].status;
+    pacP = pacman_get_position(P);
+    ghostP = G->ghost[id].pos;
+    ghostP.i = G->ghost[id].pos.i;
+    ghostP.j = G->ghost[id].pos.j;
+    static int direzione;
+    int tmp[4] = {0,1,2,3};
+
+    if (nearby_home(G->A, G->nrow, G->ncol, ghostP))
+       if (legal_dir(G, P, ghostP, UP, status, id))
+            direzione = UP;
+       
+    if (legal_dir(G, P, ghostP, direzione, status, id)) return direzione;
+
+    for (i = 0; i < 4; i++) {
+            tmp[i] = legal_dir(G, P, ghostP, i, status, id);
+    }
+
+    switch (direzione){
+        case 0:
+            if (tmp[1]) direzione = tmp[1];
+            if (tmp[3]) direzione = tmp[3];
+            if (tmp[2]) direzione = tmp[2];
+            break;
+        case 1:
+            if (tmp[0] == 0) direzione = tmp[0];
+            if (tmp[2]) direzione = tmp[2];
+            if (tmp[3]) direzione = tmp[3];
+            break;
+        case 2:
+            if (tmp[1]) direzione = tmp[1];
+            if (tmp[3]) direzione = tmp[3];
+            if (tmp[0] == 0) direzione = tmp[0];
+            break;
+        case 3:
+            if (tmp[0] == 0) direzione = tmp[0];
+            if (tmp[2]) direzione = tmp[2];
+            if (tmp[1]) direzione = tmp[1];
+            break;
+
+    }
+
+}
+
+struct position return_pos(struct ghosts* G,enum direction dir,unsigned int id) {
     struct position ghostP;
     ghostP = G->ghost[id].pos;
     int rows, cols, ghostPi, ghostPj;
@@ -158,56 +209,14 @@ struct position ghostPosition(struct ghosts* G,enum direction dir,unsigned int i
 
 //Funzione che segue pacman
 static struct position follow(struct ghosts* G, struct pacman* P, unsigned int id) {
-    int i, k;
-    struct position pacP, ghostP;
-    enum ghost_status status;
-    status = G->ghost[id].status;
-    pacP = pacman_get_position(P);
-    ghostP = G->ghost[id].pos;
-    ghostP.i = G->ghost[id].pos.i;
-    ghostP.j = G->ghost[id].pos.j;
-    static int direzione[4] = { -1,-1,-1,-1 };
-    int tmp[4] = { 0,1,2,3 };
-
-    if (nearby_home(G->A, G->nrow, G->ncol, ghostP)) { direzione[0] = UP; direzione[1] = RIGHT; direzione[2] = LEFT; direzione[3] = RIGHT; }
-
-    for (i = 0; i < 4; i++) {
-        tmp[i] = legal_movement(G, P, ghostP, i, status, id);
-    }
-    if ((pacP.j >= ghostP.j) && (pacP.i >= ghostP.i)) { direzione[0] = LEFT; direzione[1] = UP; direzione[2] = RIGHT; direzione[3] = DOWN; }
-    else if ((pacP.j <= ghostP.j) && (pacP.i <= ghostP.i)) { direzione[0] = RIGHT; direzione[1] = DOWN;  direzione[2] = LEFT; direzione[3] = UP; }
-    else if ((pacP.i >= ghostP.i) && (pacP.j <= ghostP.j)) { direzione[0] = UP; direzione[1] = LEFT;  direzione[2] = RIGHT; direzione[3] = DOWN; }
-    else if ((pacP.i <= ghostP.i) && (pacP.j >= ghostP.j)) { direzione[0] = DOWN; direzione[1] = RIGHT; direzione[2] = LEFT; direzione[3] = UP; }
-
-    for (i = 0; i < 4; i++)
-        for (k = 0; k < 4; k++)
-            if (direzione[i] == tmp[k]) ghostPosition(G, direzione[i], id);
+    //nearby_home(G);
 }
     
 
 
 //funzione per scappare da pacman, simile alla precedente funzione follow ma inversa
 static struct position escape(struct ghosts* G, struct pacman* P, unsigned int id) {
-    int i;
-    enum direction dir;
-    struct position pacP, ghostP;
-    pacP = pacman_get_position(P);
-    ghostP.i = G->ghost[id].pos.i;
-    ghostP.j = G->ghost[id].pos.j;
-    int direzione[4] = { -1,-1,-1,-1 };
-    if ((pacP.j >= ghostP.j) && (pacP.i >= ghostP.i)) { direzione[0] = LEFT; direzione[1] = UP; direzione[2] = RIGHT; direzione[3] = DOWN; }
-             else if ((pacP.j <= ghostP.j) && (pacP.i <= ghostP.i)) { direzione[0] = RIGHT; direzione[1] = DOWN;  direzione[2] = LEFT; direzione[3] = UP; }
-                    else if ((pacP.i >= ghostP.i) && (pacP.j <= ghostP.j)) { direzione[0] = UP; direzione[1] = LEFT;  direzione[2] = RIGHT; direzione[3] = DOWN; }
-                            else if ((pacP.i <= ghostP.i) && (pacP.j >= ghostP.j)) { direzione[0] = DOWN; direzione[1] = RIGHT; direzione[2] = LEFT; direzione[3] = UP; }
-    for (i = 1; i < 4; i++)
-        direzione[i] = rand() % 4;
-
-    for (i = 0; i < 4; i++) {
-        dir = direzione[i];
-        if (legal_movement(G, P, ghostP, dir, G->ghost[id].status, id))return G->ghost[id].pos;
-    }
-    return G->ghost[id].pos;
-}
+ }
 
 //funzione per ritrovare casa
 static struct position wayhome(struct ghosts* G, struct pacman* P, unsigned int id) {
